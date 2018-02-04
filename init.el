1b87b0bc305974f5ce4a995520a8be84334292c5
lnfiniteMonkeys <dim.kyriakoudis@outlook.com>
2018-02-04 13:44:32 +0000

    added use-package and archives

--- init.el
+++ init.el
@@ -1,61 +1,33 @@
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+
 (require 'package)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
- '(initial-buffer-choice t)
- '(menu-bar-mode nil)
- '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.org/packages/"))))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "GOOG" :family "Office Code Pro"))))
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa-stable" . "http://stable.melpa.org/packages/")))))
+(setq package-archives '(("melpa" . "https://stable.melpa.org/packages/")  ;;("melpa" . "http://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")))
+
+(package-initialize)
 
 
- (custom-set-variables
-   '(haskell-process-suggest-remove-import-lines t)
-   '(haskell-process-auto-import-loaded-modules t)
-   '(haskell-process-log t))
- (eval-after-load 'haskell-mode '(progn
-   (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-   (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-   (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
-   (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
-   (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
-   (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)))
- (eval-after-load 'haskell-cabal '(progn
-   (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-   (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-   (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-   (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
+;; Install use-package if it's not already installed.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;;;;;;;;;;;;;;;;;
-; buffer-stack ;;
-;;;;;;;;;;;;;;;;;
+(eval-when-compile
+  (require 'use-package)
+  (setq use-package-always-ensure t))
 
-(setq buffer-stack-show-position 'buffer-stack-show-position-buffers)
-
-(autoload 'buffer-stack-down "buffer-stack"  nil t)
-(autoload 'buffer-stack-up "buffer-stack"  nil t)
-(autoload 'buffer-stack-bury-and-kill "buffer-stack"  nil t)
-(autoload 'buffer-stack-bury "buffer-stack"  nil t)
-(eval-after-load "buffer-stack" '(require 'buffer-stack-suppl))
-
-(global-set-key [(f10)] 'buffer-stack-bury)
-(global-set-key [(control f10)] 'buffer-stack-bury-and-kill)
-(global-set-key [(f9)] 'buffer-stack-down)
-(global-set-key [(f11)] 'buffer-stack-up)
-(global-set-key [(shift f10)] 'buffer-stack-bury-thru-all)
-(global-set-key [(shift f9)] 'buffer-stack-down-thru-all)
-(global-set-key [(shift f11)] 'buffer-stack-up-thru-all)N
+(use-package haskell-mode)
+(use-package ghc-mod)
+(use-package org)
+(use-package smex)
